6735dbda7dd4c322cf2bfe8e916fbac442bff116
kabalisa innocent fiston <ikabalisa20@gmail.com>
2018-11-12 05:19:01 -0500

    branch to get all parcels

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import parcelController from '../Controllers/parcelController';
+
+const router = express.Router();
+
+router.get('/tail', (req,res) =>{
+  res.json({status: 'it works'});
+});
+
+/* GET all parcel delivery orders*/
+router.get('/parcels', parcelController.getAll);
+
+export default router;
